2f1869c7726d6cbf9a6a247c4ca5d1ecfe618853
moab7698 <moab7698@gmail.com>
2021-01-08 04:26:36 -0800

    first

--- config/leaflet.php
+++ config/leaflet.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'zoom_level'           => 13,
-    'detail_zoom_level'    => 16,
-    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-3.313695'),
-    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '114.590148'),
+    'zoom_level'           => 5,
+    'detail_zoom_level'    => 5,
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '33.730665'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '51.227532'),
 ];
---
+++ resources/lang/fa/mamad.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'Login' => 'ورود',
+    'Logout' => 'خروج',
+    'Register' => 'ثبت نام',
+    'list' => 'لیست',
+    'our_outlets' => 'نقشه',
+    'Register' => 'ثبت نام',
+    'Register' => 'ثبت نام',
+
+    // 
+    "name" => 'نام',
+    "address" => 'ادرس',
+    "latitude" => 'عرض جغرافیایی',
+    "longitude" => 'طول جغرافیایی ',
+
+    // 
+    "table_no" => 'شماره ردیف',
+
+    // 
+    "Forgot Your Password?" => "رمز عبور خو را فرامش کردید؟",
+    'Remember Me' => "من را به خاطر بسپار",
+    "Password" => "رمز",
+    "E-Mail Address" => "ایمل",
+
+
+
+
+];
